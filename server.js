bdd7a87b3b8567bc0d345bd6ed33a3bdd7e689aa
Wilhelm Hugo Kirschbaum <wkirschbaum@gmail.com>
2021-10-27 16:23:58 +0200

    Add cors and transaction list

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const crypto = require('crypto');
+const cors = require('cors')
 
 const app = express();
 
@@ -9,7 +10,7 @@ const { Client } = require('pg');
 const connectionString = 'postgresql://localhost:5432/floatpays'
 
 app.use(express.static('./'))
-
+app.use(cors({ origin: '*' }));
 
 app.get('/api/v1/transactions', async (request, response) => {
   const client = new Client({ connectionString });
--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 function App() {
+  const [transactions, setTransactions] = useState([]);
+
+  const fetchTransactions = async () => {
+    const { data } = await Axios.get('http://localhost:4000/api/v1/transactions');
+
+    setTransactions(data);
+  };
+
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
+
   return (
     <div className="App">
-      <h2>Floatpays</h2>
+    <h2>Floatpays</h2>
 
-      <hr />
+    <hr />
 
-      <h3>Transactions</h3>
+    <h3>Transactions</h3>
+
+    {transactions.map((transaction) => (
+      <p key={transaction.reference}>R{transaction.amount} - {transaction.description}</p>
+    ))}
+
     </div>
   );
 }
