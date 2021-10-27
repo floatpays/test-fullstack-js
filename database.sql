7e36e04cb00ccbe8ed7b85c431a10d1ac6aa8658
Wilhelm Hugo Kirschbaum <wkirschbaum@gmail.com>
2021-10-27 16:07:46 +0200

    Add server stuff

---
+++ database.sql
@@ -0,0 +1,19 @@
+DROP DATABASE floatpays;
+
+CREATE DATABASE floatpays;
+
+\c floatpays;
+
+create table transactions(
+  id SERIAL,
+  amount DECIMAL NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  reference VARCHAR(80) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO transactions(reference, amount, description)
+VALUES('123123', 200.12, 'Buying lunch');
+
+INSERT INTO transactions(amount, reference, description)
+VALUES('123222', 88.12, 'Coffee for my team');
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const crypto = require('crypto');
+
+const app = express();
+
+const { Client } = require('pg');
+
+const connectionString = 'postgresql://localhost:5432/floatpays'
+
+app.use(express.static('./'))
+
+
+app.get('/api/v1/transactions', async (request, response) => {
+  const client = new Client({ connectionString });
+  await client.connect();
+  client.query('select created_at, amount, reference from transactions', (err, res) => {
+    if (err) {
+      console.log(err.stack);
+      response.send([]);
+    } else {
+      response.send(res.rows);
+    }
+    client.end()
+  })
+});
+
+app.listen(process.env.port || 4000);
